952cf85965edae8753946fbd1db391db12a178a2
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-11-13 12:58:44 +0100

    Done

--- 20_JS_Strings/aufgabe1.js
+++ 20_JS_Strings/aufgabe1.js
@@ -1,2 +1,10 @@
-console ("hello")
-//test
+let a = 7
+let b = 4.4
+
+let result = a + b 
+
+//Strings//
+let firstName = "Hans"
+let lastName = "Müller"
+
+console.log( firstName + " "+ lastName + " " + result )
