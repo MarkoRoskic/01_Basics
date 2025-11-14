8783081ef594c93f9f3a785591e5f5faeb11b18c
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-11-14 15:24:34 +0100

    Done

---
+++ 40_if_Bedingung/40_2.Aufgabe.js
@@ -0,0 +1,13 @@
+let trafficLight = "green"
+
+if (trafficLight == "green") {
+    console.log("You may go");
+}
+
+if (trafficLight == "yello") {
+    console.log("Caution!");
+}
+
+if (trafficLight == "red") {
+    console.log("Stop!");
+}
