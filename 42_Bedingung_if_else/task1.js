01f793a7253250eb9c68100cad3fe3bb0a3b628c
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-11-20 15:32:13 +0100

    42/task1

---
+++ 42_Bedingung_if_else/task1.js
@@ -0,0 +1,9 @@
+let a = 10
+
+if (a >= 10) {
+    console.log("Die Zahl ist groß")
+} else if (a >= 0) {
+  console.log("Die Zahl ist positiv aber klein")
+} else {
+    console.log("Die Zahl ist negativ")
+}
