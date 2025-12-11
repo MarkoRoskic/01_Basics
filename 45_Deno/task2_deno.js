ab74cdf79cb4d756fe29361e6af322f3295c2a4c
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-12-11 13:03:59 +0100

    45/ task2

---
+++ 45_Deno/task2_deno.js
@@ -0,0 +1,19 @@
+let number = 67
+
+if (number > 0){
+    console.log("Die Zahl ist positiv")
+} else {
+    console.log("Die Zahl ist nicht positiv")
+}
+
+if (number < 0){
+    console.log("Die Zahl is negativ")
+} else {
+    console.log("Die Zahl ist postiv oder 0")
+}
+
+if (number === 0){
+    console.log("Die Zahl ist null")
+} else {
+    console.log("Die Zahl ist nicht 0")
+}
