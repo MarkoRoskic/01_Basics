b278248e52fed2e5a1bb9e30845164077623ded9
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-12-18 11:48:12 +0100

    45/ task3

---
+++ 45_Deno/task3_deno.js
@@ -0,0 +1,16 @@
+let zahl1 = prompt("zahl 1");
+let zahl2 = prompt("zahl 2");
+let rechenart = prompt("Operatoren [+,-,*,/]");
+
+if (rechenart === "+"){
+    console.log(zahl1 + zahl2);
+}
+if (rechenart === "-"){
+    console.log(zahl1 - zahl2);
+}
+if (rechenart === "*"){
+    console.log(zahl1 * zahl2);
+}
+if (rechenart === "/"){
+    console.log(zahl1 / zahl2);
+}
