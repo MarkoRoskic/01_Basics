fdf1b0987eaf8577834cb13c1ce688264311ab79
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2025-12-18 12:37:36 +0100

    45/ task4

---
+++ 45_Deno/task4_deno.js
@@ -0,0 +1,11 @@
+let Alter = parseInt(prompt("Gib dein Alter ein"))
+
+if (Alter < 25){
+    console.log("jung")
+}
+else if (Alter > 30 && Alter < 35){
+    console.log("mittelalt")
+}
+else if (Alter > 50){
+    console.log("alt")
+}
