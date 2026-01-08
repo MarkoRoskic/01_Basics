ce4c2d0dcca1a0c4ee07a200f1fa62d3c94d4cd2
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2026-01-08 12:34:31 +0100

    Finished 52 task 3

---
+++ 52_Logische_Operatoren_II/task3.js
@@ -0,0 +1,10 @@
+let grade = 85;
+let attendance = 88;
+let extraCredit = 6;
+
+if (grade >= 80 && (attendance >= 90 || extraCredit >= 5)){
+    console.log("Eligible for advanced exam");
+
+} else {
+    console.log("Not eligible for advanced exam");
+}
