2a8457f5fdd8ea45d9949874614017efa8b2000d
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2026-01-08 12:34:46 +0100

    Finished 52 task 4

---
+++ 52_Logische_Operatoren_II/task4.js
@@ -0,0 +1,12 @@
+let age = 20;
+let money = 10;
+
+if (age >= 18 && money >= 12){
+    console.log("Ticket purchased");
+
+} else if (age >= 18 && money < 12){
+    console.log("Not enough money");
+
+} else {
+    console.log("Too young to watch the movie")
+}
