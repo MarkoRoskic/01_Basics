fa2b955320db19b6c1b686ac065bf79ecce36492
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2026-01-08 12:33:28 +0100

    Finished 52 task1

---
+++ 52_Logische_Operatoren_II/task1.js
@@ -0,0 +1,12 @@
+let temperature = 30
+let humidity = 50
+
+if (temperature >= 20 && temperature <= 25 && humidity < 60){
+    console.log("Perfect day for outdoor activities!");
+
+} else {
+    console.log("Maybe stay indoors today");
+}
+
+
+
