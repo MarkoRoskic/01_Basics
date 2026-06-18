28991cc71bd70fcdc602180a648c541a5e034d04
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2026-06-18 13:07:36 +0200

    Refactored Code for JS

---
+++ 03_HTML_JS/test.js
@@ -0,0 +1,23 @@
+
+
+let dana = [
+    { firstName: "Hans", lastName: "Huber", points: 30 },
+    { firstName: "Paul", lastName: "Müller", points: 34 },
+    { firstName: "Franz", lastName: "Maier", points: 35 }
+];
+
+
+
+function loadPeople() {
+    let html = "";
+    dana.forEach(element => {
+        html += "<div>" + element.firstName + " " + element.lastName + "</div>"
+    });
+
+    console.log(html);
+
+
+
+}
+
+loadPeople();
