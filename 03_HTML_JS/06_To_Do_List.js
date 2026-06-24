7304e9620b0b908b75e70fd142c0c312b01cc4cc
MarkoRoskic <marko.roskic@student.htldornbirn.at>
2026-06-24 09:42:19 +0200

    Finished 06_To_Do_List

---
+++ 03_HTML_JS/06_To_Do_List.js
@@ -0,0 +1,56 @@
+let todos = [
+    { id: 1, title: "putzen", isDone: false },
+    { id: 2, title: "kochen", isDone: true },
+    { id: 3, title: "einkaufen", isDone: false },
+    { id: 4, title: "lernen", isDone: false },
+];
+
+let maxId= 4;
+printTasks();
+
+document.getElementById("input").addEventListener("change", function(event){
+    console.log(event.target.value);
+    maxId++; 
+    todos.push({id:maxId,title:event.target.value,isDone:false})
+    printTasks();
+    event.target.value = ""
+    
+})
+
+function printTasks(){
+    let html = "";
+todos.forEach(element => {
+    let style = element.isDone?"green":"none";
+    html += "<div class='task' style='background-color:" + style + "'>" + element.title + " - " + element.isDone + "<div onClick='setisDone(" + element.id + ")'>Done</div><divonClick='deleteTask(" + element.id + ")'>Delete</div></div>";
+});
+
+document.getElementbyId("todos").innerHTML = html; 
+
+}
+
+function deleteTask(id){
+    let index = todos.findIndex(element=>{
+        if (element.id===id){
+            return true;
+        }
+    })
+    todos.splice(index,1);
+    printTasks();
+
+}
+
+function setisDone(id){
+   let todo = todos.find(element=>{
+        if (element.id===id){
+            return true;
+        }
+    })
+    todo.isDone =!todo.isDone;
+    printTasks();
+}
+
+
+
+
+
+
